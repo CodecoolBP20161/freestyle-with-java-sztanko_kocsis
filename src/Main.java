5b0adf78996114225b5df40589b6701b9ad1ff97
dsztanko <szt.dori@gmail.com>
2016-10-25 23:37:15 +0200

    representing how json handler works

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import Controller.JsonHandler;
 import Frames.LoginFrame;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
 
 //main class to start the password manager
 
 public class Main {
 
-    public static void main(String[] args) {
-        LoginFrame login = new LoginFrame();
+    public static void main(String[] args) throws IOException, ParseException {
+//        LoginFrame login = new LoginFrame();
+
+        JsonHandler handler = new JsonHandler("kiskacsa", "élkmlkm", "nkljnkjnkj");
+        String json = handler.encodeJSON();
+        JsonHandler.writeFile(json);
+//
+//        String password = JsonHandler.decodeJSON("salted_hashed_password");
+//        System.out.println(password);
+
+        }
+
     }
-}
