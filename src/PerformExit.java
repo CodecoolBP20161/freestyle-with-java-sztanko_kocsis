74b580512f2d43792a164014c58ee44239e1866a
dsztanko <szt.dori@gmail.com>
2016-10-25 13:35:11 +0200

    handles actions

---
+++ src/PerformExit.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PerformExit implements ActionListener {
+
+    public void actionPerformed(ActionEvent actionEvent1) {
+        System.exit(0);
+
+    }
+}
